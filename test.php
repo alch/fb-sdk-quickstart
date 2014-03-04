638565d79d6dcd94a9552bc6a8c187df4555d4f4
Aldo Chiecchia <zimage@tiscali.it>
2014-03-04 16:55:28 +0100

    Update test.php

--- test.php
+++ test.php
@@ -14,12 +14,12 @@ use Symfony\Component\Yaml\Exception\ParseException;
 
 try {
     $yaml = new Parser();
-        $rootConfig = $yaml->parse(file_get_contents('./config.yml'));
+    $rootConfig = $yaml->parse(file_get_contents('./config.yml'));
 
     $facebook = new Facebook($rootConfig['config']);
     $user_id = $facebook->getUser();
 
-    if($user_id) {
+    if ($user_id) {
 
         // We have a user ID, so probably a logged in user.
         // If not, we'll get an exception, which we handle below.
@@ -28,7 +28,7 @@ try {
             $user_profile = $facebook->api('/me','GET');
             echo "Name: " . $user_profile['name'];
 
-        } catch(FacebookApiException $e) {
+        } catch (FacebookApiException $e) {
             // If the user is logged out, you can have a
             // user ID even though the access token is invalid.
             // In this case, we'll get an exception, so we'll
